b4738e1cda025f9f3a42b21ef461b2791fbb539b
shusakukamiyama <xyz000shupi000@gmail.com>
2020-01-12 22:10:25 +0900

    基本的なコンポーネントを作成

--- src/container/templates/TopPage.tsx
+++ src/container/templates/TopPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Post from '../../define/model/post/Post';
 import GetPostsTaskFactory from '../../lib/task/posts/GetPostsTask';
+import Card from '@material-ui/core/Card';
+import { CardContent, Typography } from '@material-ui/core';
 
 type Props = {};
 
@@ -32,15 +34,22 @@ export default class TopPage extends React.Component<Props, State> {
     };
 
     private renderPosts = () => {
-        const postTitle = this.state.posts.map(post => <p>{post.title}</p>);
-        return postTitle;
+        const postCards = this.state.posts.map(post => (
+            <Card style={{ margin: 10, backgroundColor: 'blue'}} onClick={() => alert("aaa")}>
+                <CardContent>
+                    <Typography variant='body2' color='textSecondary' component='p'>
+                        {post.title}
+                    </Typography>
+                </CardContent>
+            </Card>
+        ));
+        return postCards;
     };
 
     public render() {
         return (
             <div className='App'>
-                <h1>firebase連携</h1>
-                <button onClick={() => this.fetchPosts()}>データを取得</button>
+                <h1>理系就活口コミサイト</h1>
                 {this.renderPosts()}
             </div>
         );
