4353b93bb84f83d1abab8c512c936abbb1c7c783
shusakukamiyama <xyz000shupi000@gmail.com>
2020-01-12 19:33:47 +0900

    firebaseと簡単に連携

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import { appInitilize } from './lib/firebase/initialize/AppInitialize';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component<{}> {
+    public componentDidMount() {
+        //アプリケーションの初期化
+        appInitilize();
+    }
+
+    public fetchPosts = () => {
+        const db = firebase.firestore();
+        db.collection('posts')
+            .doc('GJPiU4iSqvhS6LWd3ZvS')
+            .get()
+            .then(doc => {
+              console.log(doc.data())
+            })
+    };
+
+    public render() {
+        return (
+            <div className='App'>
+                <h1>firebase連携</h1>
+                <button onClick={() => this.fetchPosts()}>データを取得</button>
+            </div>
+        );
+    }
 }
-
-export default App;
---
+++ src/lib/firebase/initialize/AppInitialize.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+export const appInitilize = () => {
+    const firebaseConfig = {
+        apiKey: 'AIzaSyBsSfcbEkOo64Z4Ay0BZHPer2tMZdcpCP4',
+        authDomain: 'jobapp-dev.firebaseapp.com',
+        databaseURL: 'https://jobapp-dev.firebaseio.com',
+        projectId: 'jobapp-dev',
+        storageBucket: 'jobapp-dev.appspot.com',
+        messagingSenderId: '134062550540',
+        appId: '1:134062550540:web:e334b434fb4a3693c20829'
+    };
+
+    firebase.initializeApp(firebaseConfig);
+};
