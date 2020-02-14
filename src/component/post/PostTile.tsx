47fc2877257e815a3037788e35c81cd0ba3886af
shusakukamiyama <xyz000shupi000@gmail.com>
2020-02-14 20:32:37 +0900

    postTileを変更

--- src/component/post/PostTile.tsx
+++ src/component/post/PostTile.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import Post from '../../define/model/post/Post';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Post from '../../define/model/post/Post';
 import { Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
@@ -14,35 +22,83 @@ type Props = {
     post: Post;
 };
 
-export const PostTile = ({post} : Props) => {
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            maxWidth: 345
+        },
+        media: {
+            height: 0,
+            paddingTop: '56.25%' // 16:9
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto',
+            transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest
+            })
+        },
+        expandOpen: {
+            transform: 'rotate(180deg)'
+        },
+        avatar: {
+            backgroundColor: red[500]
+        }
+    })
+);
+
+export const PostTile: React.FC<Props> = props => {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return (
-        <Grid item xs={3} sm={3}>
-            <Link to={`/posts/${post.id}`}>
-                <Card>
-                    <CardActionArea>
-                        {/* <CardMedia
-                            component='img'
-                            alt='Contemplative Reptile'
-                            height='140'
-                            image={require('../../static/images/startup-594091_1920.jpg')}
-                            title='Contemplative Reptile'
-                        /> */}
-                        <CardContent>
-                            <Typography gutterBottom variant='h5' component='h2'>
-                                {post.title}
-                            </Typography>
-                            <Typography variant='body2' color='textSecondary' component='p'>
-                                {post.content}
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button size='small' color='primary'>
-                            シェア
-                        </Button>
+        <Grid item xs={1} md={3} lg={6}>
+            <Link to={`/posts/${props.post.id}`}>
+                <Card className={classes.root}>
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label='recipe' className={classes.avatar}>
+                                R
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton aria-label='settings'>
+                                <MoreVertIcon />
+                            </IconButton>
+                        }
+                        title='高橋順平'
+                        // subheader={props.post.createdAt}
+                    />
+                    <CardMedia className={classes.media} image='/static/images/cards/paella.jpg' title='Paella dish' />
+                    <CardContent>
+                        <Typography variant='body2' color='textSecondary' component='p'>
+                            {props.post.content}
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label='add to favorites'>
+                            <FavoriteIcon />
+                        </IconButton>
+                        <IconButton aria-label='share'>
+                            <ShareIcon />
+                        </IconButton>
+                        <IconButton
+                            className={clsx(classes.expand, {
+                                [classes.expandOpen]: expanded
+                            })}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label='show more'
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
                     </CardActions>
                 </Card>
             </Link>
         </Grid>
     );
-}
+};
